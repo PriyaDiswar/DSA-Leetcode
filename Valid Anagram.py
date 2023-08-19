5594892d43836042eb0d14f34520ce246e14b5f2
PriyaDiswar <95042955+PriyaDiswar@users.noreply.github.com>
2023-08-19 15:41:20 +0530

    Create Valid Anagram.py

---
+++ Valid Anagram.py
@@ -0,0 +1,22 @@
+"""
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+Example:
+Input: s = "anagram", t = "nagaram"
+Output: true
+"""
+
+Solution:
+
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if sorted(s)==sorted(t):
+            return True
+        else:
+            return False
